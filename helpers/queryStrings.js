b2c0a6c3135620ba7dbffb6fede3bb75a111f6c5
Vinit Shah <elysiums.shade@gmail.com>
2013-11-21 23:56:01 -0600

    Check if email exists query

--- helpers/queryStrings.js
+++ helpers/queryStrings.js
@@ -3,5 +3,6 @@ var User = {
 ,   VERIFY_REGISTRATION_TOKEN : 'SELECT * FROM users WHERE ?'
 ,   SET_REGISTRATION_VERIFIED : 'UPDATE users SET ? WHERE ? AND `dateConfirmationSent` >= DATE_SUB(NOW(), INTERVAL 5 DAY)'
 ,   CHECK_EMAIL_CONFIRMED     : 'SELECT emailConfirmed FROM users WHERE ?'
+,   CHECK_EXISTING_EMAIL      : 'SELECT email FROM users WHERE ?'
 ,   VERIFY_LOGIN              : 'SELECT U.id, U.nickname, U.email, U.password, U.emailConfirmed FROM users U WHERE ?'
 }
