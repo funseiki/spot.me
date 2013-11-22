0778e624c4a80381eaf062f674f07268ef21fc52
Vinit Shah <elysiums.shade@gmail.com>
2013-11-21 23:58:15 -0600

    Login, logout routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,35 @@
-function routes(app) {
+var path = require('path');
+
+function routes(app, passport) {
+    // Controllers
+    var controllers = require(path.join('..', 'helpers'));
+
     // Routes
+    require('./user')(app, controllers.User);
+
+    // Login/logout
+    app.get('/failed', function(req, res) {
+        res.send("Login failed");
+    });
+
+    // The database helper will verify the user
+    app.post('/login',
+        passport.authenticate('local',
+        {
+            failureRedirect: '/failed'
+        }), function(req, res)
+        {
+            //res.redirect('/');
+            res.json("Login Successful!");
+        }
+    );
+
+    // Do these routes last
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
     app.get('/', function(req, res){
         res.send("Ello Mate");
     });
