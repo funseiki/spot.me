b1a8ff922a36caab61bb46749224e4b83c4ec76c
Vinit Shah <elysiums.shade@gmail.com>
2013-11-21 21:45:32 -0600

    More helper functions for the database wrapper

--- helpers/database.js
+++ helpers/database.js
@@ -1,6 +1,9 @@
 var config = require('../config').db,
+    bcryptParams = require('../config').bcryptParams,
     mysql = require('mysql'),
-    async = require('async');
+    async = require('async'),
+    bcrypt = require('bcrypt'),
+    crypto = require('crypto');
 
 var database = {
     pool: mysql.createPool({
@@ -13,15 +16,55 @@ var database = {
     ,   waitForConnections         : config.waitForConnections
     ,   connectionLimit            : config.connectionLimit
     }),
-    cleanPool: function(err, results, connection, callback) {
+    EndConnection: function(err, results, connection, callback) {
         if(connection) connection.release();
         callback(err, results);
+    },
+    GetConnection: function(callback) {
+        this.pool.getConnection(callback);
+    },
+    GenerateHash: function(input, callback) {
+        bcrypt.genSalt(bcryptParams.workFactor, function(err, salt)
+        {
+            if(err)
+            {
+                callback(err, null);
+                return;
+            }
+            bcrypt.hash(input, salt, function(err, hash)
+            {
+                if(err)
+                {
+                    callback(err, null);
+                    return;
+                }
+                else
+                {
+                    return callback(null, hash);
+                }
+            });
+        });
+    },
+    GenerateToken: function(callback) {
+        crypto.randomBytes(48, function(ex, buf)
+        {
+            if(ex)
+            {
+                return callback(ex);
+            }
+            else
+            {
+                var token = buf.toString('hex');
+                var clean_token = sanitizer.sanitize(token);
+                callback(null, clean_token);
+            }
+        });
     },
     Query: function(queryString, inputs, callback) {
         var locals = {};
         async.waterfall([
             // Grab a connection from the pool
-            this.pool.getConnection.bind(this.pool),
+            this.GetConnection.bind(this),
             // Do the query
             function(connection, cb) {
                 locals.connection = connection;
@@ -29,7 +72,7 @@ var database = {
             }
         // Return connection to the pool and callback the results
         ], function(err, results) {
-            this.cleanPool(err, results, locals.connection, callback);
+            this.EndConnection(err, results, locals.connection, callback);
         }.bind(this));
     }
 };
