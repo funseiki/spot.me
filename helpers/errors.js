edbaf061d319cd83a1d7866c4cddc00ff669dced
Vinit Shah <elysiums.shade@gmail.com>
2013-11-21 21:45:51 -0600

    More error strings for user controller

--- helpers/errors.js
+++ helpers/errors.js
@@ -1,6 +1,12 @@
 // File containing error strings
 var User = {
-    NO_ID: "No user with that id exists"
+    NO_ID: "No user with that id exists",
+    NO_EMAIL: "No user with that email exists",
+    NOT_CONFIRMED: "Please check your email to confirm registration",
+    BAD_PASSWORD: "Incorrect password",
+    ALREADY_CONFIRMED: "This user has already confirmed",
+    INCORRECT_TOKEN: "Incorrect Token",
+    EXPIRED_TOKEN: "The token has expired"
 }
 
 var Database = {
