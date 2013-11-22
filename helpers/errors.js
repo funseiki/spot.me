692e584384cea8ae382c79f86aeb41c3ea9c8362
Vinit Shah <elysiums.shade@gmail.com>
2013-11-21 20:49:43 -0600

    Error strings

---
+++ helpers/errors.js
@@ -0,0 +1,15 @@
+// File containing error strings
+var User = {
+    NO_ID: "No user with that id exists"
+}
+
+var Database = {
+    GENERIC: "Unable to access information at this time"
+}
+
+var Errors = {
+    User: User,
+    Database: Database
+}
+
+module.exports = Errors;
