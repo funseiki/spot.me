4cee6a6c35b8b4f761e9d2217724cce9dda7b6ed
Vinit Shah <elysiums.shade@gmail.com>
2013-12-01 14:58:17 -0800

    spot controller template

---
+++ server/helpers/spot.js
@@ -0,0 +1,15 @@
+var db = require('./database'),
+    utils = require('./utils'),
+    errors = require('./errors').User,
+    async = require('async'),
+    bcrypt = require('bcrypt'),
+    sanitizer = require('sanitizer'),
+    QueryStrings = require('./queryStrings'),
+    Message = require('./clientMessage'),
+    config = require('../config');
+
+var spot = {
+
+};
+
+module.exports = spot;
