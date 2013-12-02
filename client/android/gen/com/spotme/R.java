ac01b3f869058074ef4c6535795ee3ef9acef722
Ptu <qecbtum@gmail.com>
2013-12-01 21:54:33 -0600

    main spot ui, top ten description

--- client/android/gen/com/spotme/R.java
+++ client/android/gen/com/spotme/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int blackText=0x7f06000a;
         public static final int buttonBg=0x7f060004;
         public static final int buttonBorder=0x7f060003;
         public static final int buttonText=0x7f060005;
@@ -20,6 +21,7 @@ public final class R {
         public static final int hlText=0x7f060006;
         public static final int hlbtn=0x7f060009;
         public static final int menuBg=0x7f060000;
+        public static final int secTextColor=0x7f06000b;
         public static final int white=0x7f060007;
     }
     public static final class dimen {
@@ -32,6 +34,7 @@ public final class R {
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
         public static final int regi_item_width=0x7f040002;
+        public static final int sec_text_size=0x7f040003;
     }
     public static final class drawable {
         public static final int button_bg_round=0x7f020000;
@@ -54,28 +57,33 @@ public final class R {
         public static final int text_box_last_row=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f090015;
-        public static final int addSpot=0x7f090009;
+        public static final int action_settings=0x7f09001a;
+        public static final int addSpot=0x7f09000e;
         public static final int btn_login=0x7f090002;
         public static final int btn_signup=0x7f090004;
-        public static final int cancel=0x7f09000f;
-        public static final int clue=0x7f09000b;
-        public static final int cluelist=0x7f090006;
-        public static final int confirmPassword=0x7f090010;
+        public static final int cancel=0x7f090014;
+        public static final int clue=0x7f090010;
+        public static final int cluelist=0x7f09000b;
+        public static final int confirmPassword=0x7f090015;
         public static final int control=0x7f090005;
         public static final int email=0x7f090000;
-        public static final int imageView1=0x7f090007;
-        public static final int img=0x7f090012;
-        public static final int leaderboard=0x7f09000a;
+        public static final int imageView1=0x7f09000c;
+        public static final int img=0x7f090017;
+        public static final int leaderboard=0x7f09000f;
         public static final int orText=0x7f090003;
         public static final int password=0x7f090001;
-        public static final int preview=0x7f09000d;
-        public static final int profile=0x7f090008;
-        public static final int sendCreateRequest=0x7f09000e;
-        public static final int spotId=0x7f090013;
-        public static final int takePhoto=0x7f09000c;
-        public static final int username=0x7f090011;
-        public static final int verify=0x7f090014;
+        public static final int preview=0x7f090012;
+        public static final int profile=0x7f09000d;
+        public static final int sendCreateRequest=0x7f090013;
+        public static final int spotId=0x7f090018;
+        public static final int takePhoto=0x7f090011;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090007;
+        public static final int textView3=0x7f090008;
+        public static final int textView4=0x7f090009;
+        public static final int textView5=0x7f09000a;
+        public static final int username=0x7f090016;
+        public static final int verify=0x7f090019;
     }
     public static final class layout {
         public static final int clue_list=0x7f030000;
@@ -96,9 +104,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int cancel=0x7f050011;
-        public static final int clueHint=0x7f050013;
+        public static final int clueHint=0x7f050016;
         public static final int confirmPasswordHint=0x7f05000f;
         public static final int emailHint=0x7f05000c;
+        public static final int endUnlockedMsg=0x7f050014;
         public static final int error_duplicate_email=0x7f050003;
         public static final int error_duplicate_username=0x7f050004;
         public static final int error_field_required=0x7f050008;
@@ -108,12 +117,14 @@ public final class R {
          */
         public static final int error_invalid_email=0x7f050002;
         public static final int error_invalid_password=0x7f050006;
-        public static final int newSpotTitle=0x7f050012;
+        public static final int newSpotTitle=0x7f050015;
         public static final int orText=0x7f05000b;
         public static final int passwordHint=0x7f05000e;
         public static final int register=0x7f050010;
         public static final int signIn=0x7f050009;
         public static final int submit=0x7f05000a;
+        public static final int topTenMsg=0x7f050012;
+        public static final int unlockedMsg=0x7f050013;
         public static final int usernameHint=0x7f05000d;
     }
     public static final class style {
