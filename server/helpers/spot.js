2ce8210a1032bd60ce6da477379393919d05141f
Vinit Shah <elysiums.shade@gmail.com>
2013-12-02 02:11:56 -0600

    Spot creationing!

--- server/helpers/spot.js
+++ server/helpers/spot.js
@@ -45,19 +45,73 @@ var spot = {
             },
             async.apply(db.GenerateToken),
             function(token, callback) {
-                knoxClient.putFile(locals.clean_image.imagePath, '/'+ s3Folder+'/' + token + path.extname(locals.clean_image.imageName), {'x-amz-acl': 'public-read'}, function(err, res){
+                locals.url = '/'+ s3Folder+'/' + token + path.extname(locals.clean_image.imageName);
+                knoxClient.putFile(locals.clean_image.imagePath, locals.url, {'x-amz-acl': 'public-read'}, function(err, res){
                   // Always either do something with `res` or at least call `res.resume()`.
                     if(res) res.resume();
                     callback(err, res);
                 }).on('error', function(e){ console.log("Error: " + e.message); });
             }
         ], function(err, res){
-            main_callback(err, res);
+            main_callback(err, "https://s3.amazonaws.com/" +config.aws.bucket + locals.url);
         });
     },
     create: function(user, spot, main_callback) {
-        this.makeImage(spot, 'spots', function(err, result) {
-            main_callback(err, result);
+        var locals = {};
+        // TODO: Add story to this at some point maybe
+        var required_inputs = ['imageName', 'imagePath', 'latitude', 'longitude', 'story'];
+        async.waterfall([
+            function(callback) {
+                utils.cleanInputs(spot, callback);
+            },
+            function(clean_inputs , callback) {
+                locals.clean_inputs = clean_inputs;
+                // Check for missing inputs (including for 'clue')
+                utils.inputsMissing(required_inputs, clean_inputs, function(missing){
+                    if(missing.length > 0) {
+                        callback(true);
+                    }
+                    else {
+                        callback();
+                    }
+                });
+            },
+            function(callback) {
+                utils.pruneSome(required_inputs, locals.clean_inputs, function(pruned){
+                    locals.clean_spot = pruned;
+                    locals.clue = locals.clean_inputs.clue;
+                    db.StartTransaction(callback);
+                });
+            },
+            function(connection, callback) {
+                locals.connection = connection;
+                this.makeImage({imageName: locals.clean_spot.imageName, imagePath: locals.clean_spot.imagePath}, 'spots', callback);
+            }.bind(this),
+            function(url, callback) {
+                var new_spot = {
+                    creatorID: user.id,
+                    story: locals.clean_spot.story,
+                    picture: url,
+                    latitude: locals.clean_spot.latitude,
+                    longitude: locals.clean_spot.longitude
+                };
+                locals.connection.query(QueryStrings.Spot.CREATE, new_spot, callback);
+            },
+            function(row, fields, callback) {
+                var new_clue = {
+                    spotID: row.insertId,
+                    message: locals.clue,
+                    order: 0
+                }
+                locals.connection.query(QueryStrings.Clue.CREATE, new_clue, callback);
+            }
+        ], function(err, result) {
+            if(locals.connection) {
+                db.EndTransaction(err, result, locals.connection, main_callback);
+            }
+            else {
+                main_callback(err, result);
+            }
         });
     }
 };
