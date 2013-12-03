daa712aaaf893d2329f3553a4bb1fb3a4f604236
Ptu <qecbtum@gmail.com>
2013-12-03 17:21:21 -0600

    Profile

--- client/android/src/com/spotme/Login.java
+++ client/android/src/com/spotme/Login.java
@@ -19,7 +19,7 @@ public class Login extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login);
+		setContentView(R.layout.profile);
 		loginButtonSetup();
 		signupButtonSetup();
 	}
